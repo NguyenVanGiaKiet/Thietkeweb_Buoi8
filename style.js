95001c575c16338037ef202326ad361448db961d
Kiet <fancr7qwer@gmail.com>
2022-04-04 10:49:59 +0700

    add

--- style.js
+++ style.js
@@ -1,6 +1,23 @@
-function myFunction1() {
-    document.getElementById("input").reset();
-  }
-  function myFunction2() {
-    document.getElementById("input").Generate();
-  }
+const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*_-+=";
+const passwordTxt = document.getElementById("checkbox1"); 
+const length = document.getElementById("checkbox2");
+const incNumbers = document.getElementById("checkbox3");
+const incSymbols = document.getElementById("checkbox4");
+const generateBtn = document.getElementById("generate");
+generateBtn.addEventListener("click", () => {
+    let characters = alpha;
+    incNumbers.checked ? (characters += numbers) : "";
+    incSymbols.checked ? (characters += symbols) : "";
+    passwordTxt.value = generatePassword(length.value, characters);
+  });
+  const generatePassword = (length, characters) => {
+    let password = "";
+    for (let i = 0; i < length; i++) {
+      password += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+    return password;
+  };
